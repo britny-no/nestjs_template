d872b3f0affec478ae298f3e80ec12e7cc3411cc
tinker-britny <njw990@tinker.style>
2025-01-17 12:06:06 +0900

    refactor: dotenv-vault dropped and refactoring some code for github actions

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,12 +1,6 @@
 import { ConfigModule } from '@nestjs/config';
-import * as path from 'path';
 
 export const EnvConfig = ConfigModule.forRoot({
-  envFilePath: path.join(
-    __dirname,
-    '../../envs/',
-    `.env.${process.env.NODE_ENV || 'qa'}`,
-  ),
   isGlobal: false, //모듈에서 사용시 import하는 통일성 위해 False
   cache: true,
 });
